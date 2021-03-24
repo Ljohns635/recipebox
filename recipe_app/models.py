e03ff1991b77ae8c621adfb6c8358dbb6c157dee
Latisha Johnson <latishaj966@gmail.com>
2021-03-24 09:49:26 -0700

    added createdat

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Author(models.Model):
     author_name = models.CharField(max_length=150)
@@ -16,6 +17,7 @@ class RecipeItems(models.Model):
     description = models.TextField()
     approx_time = models.CharField(max_length=20)
     instructions = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.title} | {self.author}"
